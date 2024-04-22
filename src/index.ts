5e449155bf01b4658049d86eb93adf30b4226d16
HersiYussuf <teslaaaa369@gmail.com>
2024-04-22 14:26:38 +0300

    gragphql initials

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
