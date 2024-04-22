45256e2a361c6e5e40f31acdebf2bce75ce9e2df
HersiYussuf <teslaaaa369@gmail.com>
2024-04-23 00:54:57 +0300

    Add GraphQL schema and update index.ts to use the schema from a file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
-// TODO
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { readFileSync } from "fs";
 import path from "path";
 import { gql } from "graphql-tag";
+
+// Correctly load and use the GraphQL schema from a file
+const schemaPath = path.resolve(__dirname, "./schema.graphql");
+const typeDefs = gql(readFileSync(schemaPath, { encoding: "utf-8" }));
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs })
+  const { url } = await startStandaloneServer(server, { listen: { port: 5000 } }); // Ensure to specify the port or other options if necessary
+  console.log(`
+        🚀  Server is running!
+        📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
